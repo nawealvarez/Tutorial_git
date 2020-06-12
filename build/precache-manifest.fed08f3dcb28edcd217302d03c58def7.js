ce5518d7bbcdeb9cdcc470938903afb7910ab239
nawealvarez <37459045+nawealvarez@users.noreply.github.com>
2020-06-11 22:06:28 -0300

    build for backend

---
+++ build/precache-manifest.fed08f3dcb28edcd217302d03c58def7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3340ac739a9ff50ce19cb864dfe9e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a329a414935a81770ec",
+    "url": "/static/css/main.9afd7aa0.chunk.css"
+  },
+  {
+    "revision": "54a6770e777097dce867",
+    "url": "/static/js/2.39094453.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39094453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a329a414935a81770ec",
+    "url": "/static/js/main.1af7a97f.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  }
+]);
